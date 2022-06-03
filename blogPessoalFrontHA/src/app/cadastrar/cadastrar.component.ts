d075eb85eb69a5985aafdd262c91e56934e86a4d
Helga <onegra138@gmail.com>
2022-06-02 23:32:43 -0300

    consumo de Api login e cadastro

--- blogPessoalFrontHA/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoalFrontHA/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+confirmSenha: string
+tipoUser: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
 
+  confirmarSenha(event: any) {
+this.confirmSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+this.tipoUser = event.target.value
+  }
+
+  cadastrar() {
+    this.user.tipo = this.tipoUser
+
+    if(this.user.senha != this.confirmSenha ){
+      alert('Senhas incorretas')
+    } else{
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso.')
+      })
+
+    }
+  }
+ 
 }
