1e2cfce18fc7ecdc43ca6d2d057955f36fdaf369
Helga <onegra138@gmail.com>
2022-06-08 19:36:02 -0300

    componente inicio sem consumo

--- blogPessoalFrontHA/src/app/app.module.ts
+++ blogPessoalFrontHA/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- blogPessoalFrontHA/src/app/entrar/entrar.component.ts
+++ blogPessoalFrontHA/src/app/entrar/entrar.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
-import { UserLogin } from '../model/UserLogin';
+import { UserLogin } from './../model/UserLogin';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment.prod';
 
 @Component({
   selector: 'app-entrar',
@@ -11,39 +11,30 @@ import { AuthService } from '../service/auth.service';
 })
 export class EntrarComponent implements OnInit {
 
-
-userLogin: UserLogin = new UserLogin
-
+  userLogin: UserLogin = new UserLogin()
   constructor(
     private auth: AuthService,
     private router: Router
   ) { }
-
   ngOnInit(){
     window.scroll(0,0)
   }
 
   entrar() {
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
-      this.userLogin = resp
-
-environment.token = this.userLogin.token
-environment.nome = this.userLogin.nome
-environment.foto = this.userLogin.foto
-environment.id = this.userLogin.id
-
-
-
-
-
-      this.router.navigate(['/inicio'])
-    }, erro =>{
-        if(erro.status == 500){
-          alert('usuário ou senha estão incorretos')
+    this.auth.entrar(this.userLogin).subscribe({
+      next: (resp: UserLogin)=>{
+        this.userLogin = resp
+        environment.token = this.userLogin.token
+        environment.nome = this.userLogin.nome
+        environment.foto = this.userLogin.foto
+        environment.id = this.userLogin.id
+        this.router.navigate(['/inicio'])
+      },
+      error: erro => {
+        if(erro.status == 401){
+          alert(' Usuário ou senha incorretos!')
         }
-
-    })
-    }
+      },
+    });   
   }
-
-
+}
